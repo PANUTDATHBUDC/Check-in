a7155eba2c30f46ae78441f0ecb43d856b00c5d2
PANUTDATHBUDC <PANUTDA.TH.BUDC@cpird.in.th>
2025-09-11 15:12:56 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,9 +1,10 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyCvAPSmpgklV7ve5ydmbBWj6S7Rs1nc7bL4zr_21Pk_E41SFVzKlgWE0fa5d2Q-Blh/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwxtZeT06LU66ds8A43R-k5pUHqVE2KP1VzP33oIzvAFLcideIXXJ7JAce2w2veiQbI/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxD3MGrWQKKGuXHcTletTPwOwxLgrDVDjf5r63cdHCBt4gaG64GdDDA78I2ybHTDuzOYg/exec';
 //LIFF_ID
     const LIFF_ID = '2008088002-dm72Knnm'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008088002-myBlpww1'; // LIFF ID history
     const LIFF_REGISTER = '2008088002-YlRylZZw'; // LIFF ID register
+
 
 
